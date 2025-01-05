ec3ccbd52bae929fcd6b7f440319570901be4188
DKormann <DKormann@users.noreply.github.com>
2025-01-05 00:14:39 +0000

    deploy: 2aee5237c50f77dc735f1139be00ae7409a42d83

--- userscripts/eater.js
+++ userscripts/eater.js
@@ -1,42 +1,36 @@
 
-let green = '#ff4400'
+let color = '#ff4400'
 let tail = []
 
-async function walk(dx, dy){
-  
+let direction = [1,0]
+let speed = 2
+let lock = false
+
+async function walk(){
+  if (lock) return
+  lock = true
   const x = state.player.position.x
   const y = state.player.position.y
-  const endx = x + dx
-  const endy = y + dy
+  const endx = x + direction[0] * speed
+  const endy = y + direction[1] * speed
 
   if(state.world[endx][endy] != null) await action({action: 'delete', x:endx, y:endy})
-  if (!await action({action: 'move', x, y, endx, endy})) return
-
-  await create(x, y, green)
+  if (await action({action: 'move', x, y, endx, endy})) create(x, y, color)
+  lock = false
 }
 
 async function create (x, y, color){
   return await action({action: 'put', x:x, y:y, color})
 }
 
-let speed = 1
-
-
-let direction = [0,speed]
-
-
-setInterval(() => {
-  if (direction[0] === 0 && direction[1] === 0) return
-  walk(...direction)
-}, 100);
-
-
 document.addEventListener('keydown', e => {
-  if (e.key === 'ArrowUp') direction = [0, -speed]
-  else if (e.key === 'ArrowDown') direction = [0, speed]
-  else if (e.key === 'ArrowLeft') direction = [-speed, 0]
-  else if (e.key === 'ArrowRight') direction = [speed, 0]
+  if (e.key === 'ArrowUp') direction = [0, -1]
+  else if (e.key === 'ArrowDown') direction = [0, 1]
+  else if (e.key === 'ArrowLeft') direction = [-1, 0]
+  else if (e.key === 'ArrowRight') direction = [1, 0]
+  walk()
 })
+
 document.addEventListener('keyup', e => {
-  if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key)) direction = [0,0]
+  if (e.key.startsWith("Arrow")) direction = [0,0]
 })
